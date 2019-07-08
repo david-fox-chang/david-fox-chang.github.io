bb4b88340da9f101d4958f1a9161baa4cd3d0d0e
David <david.fox0904@gmail.com>
2019-07-08 08:21:36 +0800

    prepare dev: app entry-point vue-file created

---
+++ the-f2e-2019/src/App.vue
@@ -0,0 +1,8 @@
+<template><router-view id="app"/></template>
+
+<style lang="less">
+@import './styles/reset.css';
+@import './styles/reset-fixed.css';
+@import './styles/noto-tc.css';
+@import './styles/global.less';
+</style>
---
+++ the-f2e-2019/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <h1 class="header">大衛就是小狐狸 - 精神時光屋 2019</h1>
+    <div class="nav">
+      <router-link
+        v-for="({ path, name }, index) in routes"
+        :to="path"
+        :key="`home-nav-${index}`"
+      >{{ getName(name) }}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import routes from '@routes';
+
+export default {
+  name: 'home',
+  data: () => ({
+    routes,
+  }),
+  methods: {
+    getName(name) {
+      return name;
+    },
+  },
+};
+</script>
