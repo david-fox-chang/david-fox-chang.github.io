50f031faa41aee838cd15e40e96f9d120d0db143
David <david.fox0904@gmail.com>
2019-07-09 11:27:57 +0800

    import/no-cycle logical error fixed

--- the-f2e-2019/src/catalog/views/Home.vue
+++ the-f2e-2019/src/catalog/views/Home.vue
@@ -4,7 +4,7 @@
     <h1 class="header">大衛就是小狐狸 - 精神時光屋 2019</h1>
     <div class="nav">
       <router-link
-        v-for="({ path, name }, index) in routes"
+        v-for="({ path, name }, index) in $router.options.routes"
         :to="path"
         :key="`home-nav-${index}`"
       >{{ getName(name) }}</router-link>
@@ -13,14 +13,9 @@
 </template>
 
 <script>
-// eslint-disable-next-line import/no-cycle
-import routes from '@routes';
 
 export default {
   name: 'home',
-  data: () => ({
-    routes,
-  }),
   methods: {
     getName(name) {
       return name;
